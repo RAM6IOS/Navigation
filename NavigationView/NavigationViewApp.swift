131187787cd4f237ed5ec906f8fc52d7713843de
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-03-18 11:59:26 +0100

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by MAC on 18/3/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
